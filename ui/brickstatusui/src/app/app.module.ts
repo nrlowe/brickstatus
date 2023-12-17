b25587180f7f9653cb9545aec3e40fed9e9fba12
nrlowe <nate.lowe.2013@owu.edu>
2023-12-17 15:45:31 -0500

    Adding home component + route

--- ui/brickstatusui/src/app/app.module.ts
+++ ui/brickstatusui/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
